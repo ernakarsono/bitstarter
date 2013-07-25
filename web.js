1e577a82361da39d07b7069b8c36ac3e2688a39a
ernakarsono <e_karsono@hotmail.com>
2013-07-25 16:04:32 +0000

    250713

--- web.js
+++ web.js
@@ -2,10 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
+//app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
 
+fs.readFileSync('index.html')
+
+response.send(fs.toString());
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
